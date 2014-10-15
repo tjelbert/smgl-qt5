aa142ee9465cf29975b4f632550c06d2fec25782
Treeve Jelbert <treeve@sourcemage.org>
2014-10-15 23:11:16 +0200

    simplify ma/plasma-desktop/plasma.sh

--- plasma/plasma-desktop/plasma.sh
+++ plasma/plasma-desktop/plasma.sh
@@ -2,23 +2,17 @@
 if [ "$USER" != "root" ];then
 # setup KDE5 environment for SMGL
 QTDIR=/opt/qt5
-export KF5=$QTDIR
+KF5=$QTDIR
 export PATH=$KF5/bin:$PATH
 export XDG_CONFIG_DIRS=/etc/xdg
-export XDG_DATA_DIRS=$KF5/share:/usr/share
-#export XDG_CONFIG_HOME=$HOME/.kde5
-export QT_PLUGIN_PATH=$KF5/lib/plugins:$QTDIR/plugins:$QT_PLUGIN_PATH
+export XDG_DATA_DIRS=/usr/share
+export QT_PLUGIN_PATH=$KF5/lib/plugins
+#export QT_PLUGIN_PATH=$KF5/lib/plugins:$QTDIR/plugins:
 
 export QML2_IMPORT_PATH=$KF5/lib/qml
 export QML_IMPORT_PATH=$QML2_IMPORT_PATH
-export KDEDIR=$QTDIR
-export KDE_SESSION_VERSION=5
-export KDE_FULL_SESSION=true
-
-export XDG_DATA_HOME=$HOME/.local5
-export XDG_CONFIG_HOME=$HOME/.config5
-export XDG_CACHE_HOME=$HOME/.cache5
-export XCURSOR_PATH=$KF5/share/icons:~/.icons:/usr/share/icons:/usr/share/pixmaps
+export XCURSOR_PATH=~/.icons:/usr/share/icons:/usr/share/pixmaps
+#export XCURSOR_PATH=$KF5/share/icons:~/.icons:/usr/share/icons:/usr/share/pixmaps
 
 # temporary runtime directories
 export XDG_RUNTIME_DIR=${TMPDIR-/tmp}/plasma-$USER
